1d2d6ad46c640171cf429a11c4140d5abb1a7357
cwatwate <cwatwate@ucsc.edu>
2018-07-11 13:50:26 -0700

    constructs bar graph of points in bins

--- Python/graphing2D.py
+++ Python/graphing2D.py
@@ -16,7 +16,7 @@ def randrange(n, vmin, vmax):
 	@param vmax - maximum random number
 	'''
 	#np.random.rand() gives an array of randoms
-	return (vmax - vmin) * np.random.normal(.5, .1, xDim)
+	return (vmax - vmin) * np.random.normal(.5, .1, xDim) + vmin
 
 def makebins(xvals, yvals):
 	summ = 0
@@ -27,29 +27,57 @@ def makebins(xvals, yvals):
 	print('actual mean', (summ/len(xvals)))
 	return bins
 
+def normalize(dataset):
+	for i in range(len(dataset)):
+		dataset[i] = (50 * (dataset[i] / 150))
+	return dataset
+
+def generatespots(numSpots, numPoints):
+	pointsList = [[numPoints * numSpots], [numPoints * numSpots]]
+	for i in range(numSpots):
+		rand = np.random.rand(1)[0]
+		print(rand)
+		'''
+		spotx = randrange(xDim, 0, yDim * rand)
+		spoty = randrange(xDim, 0, yDim * rand)
+		xvals = np.concatenate((spot1x, spot2x, spot3x))
+		yvals = np.concatenate((spot1y, spot2y, spot3y))
+		'''
+	
 # construct random points
 
 # first spot
-xvals = randrange(xDim, 0, yDim*2)
-yvals = randrange(xDim, 0, yDim*2)
+spot1x = randrange(xDim, 0, yDim)
+spot1y = randrange(xDim, 0, yDim)
 
 # second spot
-xvals1 = randrange(xDim, 0, yDim)
-yvals1 = randrange(xDim, 0, yDim)
+spot2x = randrange(xDim, 0, yDim)
+spot2y = randrange(xDim, 0, yDim)
+
+# second spot
+spot3x = randrange(xDim, 0, yDim / 4)
+spot3y = randrange(xDim, 0, yDim / 4)
+
+# get set of points
+xvals = np.concatenate((spot1x, spot2x, spot3x))
+yvals = np.concatenate((spot1y, spot2y, spot3y))
+
+generatespots(5,5)
 
 # label graph
 plt.ylabel('Height')
 plt.xlabel('Width')
 
 plt.plot(xvals, yvals, 'r.', markersize=1)
-plt.plot(xvals1, yvals1, 'b.', markersize=1)
-##plt.axis([0, numbins, 0, numbins])
-
-blo = makebins(xvals1, yvals1)
-N = numBins
-x = np.arange(N)
+plt.axis([0, xDim, 0, yDim])
+print(xvals,yvals)
+blo = makebins(xvals, yvals)
+x = np.arange(numBins) * binWidth
+print(blo)
 width = 1/1.5
-##plt.bar(x, blo, width, color="blue")
+plt.bar(x, blo, 8, color="blue")
 
-line.liner()
+
+lineee = line.line(1, 200)
+print(lineee.yfromx(10))
 plt.show()
